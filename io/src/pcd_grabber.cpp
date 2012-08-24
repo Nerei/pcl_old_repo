Nerei/pcl_old_repo

commit 832af01c1c37dd4009a5778ffc708e958a2761dd

* fixed Bug #798: pcd_grabber.cpp multi close (thanks Marco!)
git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6944 a9d63959-f2ad-4865-b262-bf0e56cfafb6

io/src/pcd_grabber.cpp
@@ -145,8 +145,7 @@ pcl::PCDGrabberBase::PCDGrabberImpl::readAhead ()
   // Check if we're still reading files from a TAR file
   if (tar_fd_ != -1)
   {
-    if (!readTARHeader ())
-      closeTARFile ();
+    readTARHeader ();
     valid_ = (reader.read (tar_file_, next_cloud_, origin_, orientation_, pcd_version, tar_offset_) == 0);
     if (!valid_)
       closeTARFile ();
@@ -171,8 +170,7 @@ pcl::PCDGrabberBase::PCDGrabberImpl::readAhead ()
       {
         if (openTARFile (*pcd_iterator_) >= 0)
         {
-          if (!readTARHeader ())
-            closeTARFile ();
+          readTARHeader ();
           tar_file_ = *pcd_iterator_;
           valid_ = (reader.read (tar_file_, next_cloud_, origin_, orientation_, pcd_version, tar_offset_) == 0);
           if (!valid_)

commit 102be373d7a1b624a60616f404a5f9e0fbcbf901

fixed a mistake introduced in r6944
git-svn-id: svn+ssh://svn.pointclouds.org/pcl/trunk@6945 a9d63959-f2ad-4865-b262-bf0e56cfafb6

io/src/pcd_grabber.cpp
@@ -145,7 +145,8 @@ pcl::PCDGrabberBase::PCDGrabberImpl::readAhead ()
   // Check if we're still reading files from a TAR file
   if (tar_fd_ != -1)
   {
-    readTARHeader ();
+    if (!readTARHeader ())
+      return;
     valid_ = (reader.read (tar_file_, next_cloud_, origin_, orientation_, pcd_version, tar_offset_) == 0);
     if (!valid_)
       closeTARFile ();
@@ -166,22 +167,18 @@ pcl::PCDGrabberBase::PCDGrabberImpl::readAhead ()
       valid_ = (reader.read (*pcd_iterator_, next_cloud_, origin_, orientation_, pcd_version) == 0);
 
       // Has an error occured? Check if we can interpret the file as a TAR file first before going onto the next
-      if (!valid_)
+      if (!valid_ && openTARFile (*pcd_iterator_) >= 0 && readTARHeader ())
       {
-        if (openTARFile (*pcd_iterator_) >= 0)
+        tar_file_ = *pcd_iterator_;
+        valid_ = (reader.read (tar_file_, next_cloud_, origin_, orientation_, pcd_version, tar_offset_) == 0);
+        if (!valid_)
+          closeTARFile ();
+        else
         {
-          readTARHeader ();
-          tar_file_ = *pcd_iterator_;
-          valid_ = (reader.read (tar_file_, next_cloud_, origin_, orientation_, pcd_version, tar_offset_) == 0);
-          if (!valid_)
+          tar_offset_ += (tar_header_.getFileSize ()) + (512 - tar_header_.getFileSize () % 512);
+          int result = static_cast<int> (pcl_lseek (tar_fd_, tar_offset_, SEEK_SET));
+          if (result < 0)
             closeTARFile ();
-          else
-          {
-            tar_offset_ += (tar_header_.getFileSize ()) + (512 - tar_header_.getFileSize () % 512);
-            int result = static_cast<int> (pcl_lseek (tar_fd_, tar_offset_, SEEK_SET));
-            if (result < 0)
-              closeTARFile ();
-          }
         }
       }
 